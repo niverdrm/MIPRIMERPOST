91ac32c7da2b68824e2305a0f86a9c45383bb844
niverdrm <niverdromero12@gmail.com>
2017-10-21 12:26:05 -0500

    first commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+def post_list(request):
+	return render(request, 'blog/post_list.html', {})
+
---
+++ niver/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nose/Escritorio/django/niver/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
